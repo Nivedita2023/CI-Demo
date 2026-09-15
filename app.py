5365f799dba4af54b20f87cebc546ab2a7efa4a4
Nivedita Ranjan <niveditaranjan1998@gmail.com>
2026-09-15 19:17:23 +0530

    division added

--- app.py
+++ app.py
@@ -9,9 +9,12 @@ def sub(a, b):
 def mul(a, b):
     return a*b
 
+def divide(a, b):
+    return a/b
+
 
 if __name__ == "__main__":
-    result1 = add(4, 5)
-    result2 = sub(4, 1)
+    result1 = add(10, 5)
+    result2 = sub(4, 2)
 
     print(result1, result2)
